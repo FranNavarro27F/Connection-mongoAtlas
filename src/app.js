11a9ed9ee4680d0dd11d6f3510f9a29fb5492874
Francisco Navarro <fran27_navarro@hotmail.com>
2023-03-31 19:46:58 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+const { MONGO_ATLAS_USER, MONGO_ATLAS_PASSWORD } = process.env;
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server listeng on port 8080");
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://fran27dev:${MONGO_ATLAS_PASSWORD}@codercluster.p2becio.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to DB DBaaS");
+  });
